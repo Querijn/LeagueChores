f391a50da16f8d388956bfc4a233409a688b708f
Querijn Heijmans <fork@irule.at>
2022-01-28 17:03:24 +0100

    Remove mission data, replace with inventory item

---
+++ LCU/InventoryItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueChores
+{
+	internal class InventoryItem
+	{
+		public string inventoryType { get; set; }
+		public int itemId { get; set; }
+		public string ownershipType { get; set; }
+		public string purchaseDate { get; set; }
+		public int quantity { get; set; }
+		public string uuid { get; set; }
+
+		public static async Task<InventoryItem[]> Get(string inventoryType)
+		{
+			var response = await LCU.GetAs<InventoryItem[]>("/lol-inventory/v2/inventory/" + inventoryType);
+			return response.ok ? response.body : new InventoryItem[0] { };
+		}
+	}
+
+}
